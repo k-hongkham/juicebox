b753efa7682ec0266160f5186382ed898cc4ffdf
k-hongkham <k.hongkham@gmail.com>
2022-03-23 11:48:45 -0400

    Moved into node.

--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,12 @@
-// grab our client with destructuring from the export in index.js
-const { client } = require('./index');
+const { client } = require("./index");
 
 async function testDB() {
   try {
-    // connect the client to the database, finally
     client.connect();
 
-    // queries are promises, so we can await them
-    const result = await client.query(`SELECT * FROM users;`);
+    const { rows } = await client.query(`SELECT * FROM users;`);
 
-    // for now, logging is a fine way to see what's up
-    console.log(result);
+    console.log(rows);
   } catch (error) {
     console.error(error);
   } finally {
