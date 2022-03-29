1c125f49db64de973f1d67168ae294956bcda367
k-hongkham <k.hongkham@gmail.com>
2022-03-29 10:53:47 -0400

    added utils.js

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts } = require("../db");
+const { requireUser } = require("./utils");
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -8,12 +9,37 @@ postsRouter.use((req, res, next) => {
   next(); // THIS IS DIFFERENT
 });
 
-postsRouter.get('/', async (req, res) => {
-    const posts = await getAllPosts();
+postsRouter.get("/", async (req, res) => {
+  const posts = await getAllPosts();
 
   res.send({
-    posts
+    posts,
   });
+});
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    const postData = { authorId, title, content };
+    const post = await createPost(postData);
+
+    res.send({ post });
+    // otherwise, next an appropriate error object
+
+    //BOOKMARK - PART 3 WRITING POST
+    next(error);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;
