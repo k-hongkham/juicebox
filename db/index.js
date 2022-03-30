8b2aeb9d53dea15e489ce253d1cecc2a0edafda5
k-hongkham <k.hongkham@gmail.com>
2022-03-30 11:21:27 -0400

    Commented out client in index.js db.

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
 const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client({
-  connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : undefined,
-});
+// const client = new Client({
+//   connectionString:
+//     process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : undefined,
+// });
 
 // USER METHODS
 
