eaa343048830cc66bc033e5e55cc209d1db776f2
k-hongkham <k.hongkham@gmail.com>
2022-03-30 10:52:45 -0400

    installing and committing to Heroku.

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const PORT = 3000;
-const express = require('express');
+const { PORT = 3000 } = process.env;
+const express = require("express");
 const server = express();
-const apiRouter = require('./api');
-const morgan = require('morgan');
-server.use(morgan('dev'));
-require('dotenv').config();
+const apiRouter = require("./api");
+const morgan = require("morgan");
+server.use(morgan("dev"));
+require("dotenv").config();
 
-server.use(express.json())
-server.use('/api', apiRouter);
+server.use(express.json());
+server.use("/api", apiRouter);
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-  server.get('/add/:first/to/:second', (req, res, next) => {
-    res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+  next();
+});
+
+server.get("/add/:first/to/:second", (req, res, next) => {
+  res.send(
+    `<h1>${req.params.first} + ${req.params.second} = ${
       Number(req.params.first) + Number(req.params.second)
-     }</h1>`);
-  });
+    }</h1>`
+  );
+});
 
-const { client } = require('./db');
+const { client } = require("./db");
 
 client.connect();
 
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
